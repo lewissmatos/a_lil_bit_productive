4a0277a7387ee8f2ef983578c6e6e7e82d26a534
Lewis S. Matos <lewiismb1@gmail.com>
2024-03-15 18:01:07 -0400

    del kilo

--- lib/presentation/views/reminders/reminders_view.dart
+++ lib/presentation/views/reminders/reminders_view.dart
@@ -174,85 +174,154 @@ class ReminderItemState extends ConsumerState<ReminderItem> {
       context.push('/base/0/reminder/${widget.reminder.id}');
     }
 
-    return GestureDetector(
-      onTap: onGoToReminderDetails,
-      child: Card(
-        elevation: 0,
-        color: reminderAccentColor.withOpacity(0.2),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.reminder.title,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                        if (widget.reminder.description != null)
+    Future<bool> confirmDismiss() async {
+      return await showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Confirm"),
+                content:
+                    const Text("Are you sure you want to delete this item?"),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: const Text("DELETE")),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: const Text("CANCEL"),
+                  ),
+                ],
+              );
+            },
+          ) ??
+          false; // In case the user dismisses the dialog by clicking away from it
+    }
+
+    return Dismissible(
+      key: Key(widget.reminder.id
+          .toString()), // Unique identifier for Dismissible widget
+      // onDismissed: (direction) {
+      //   onMarkReminderAsDone();
+      // },
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.startToEnd) {
+          onMarkReminderAsDone();
+          return false;
+        }
+        return await confirmDismiss();
+      },
+      background: Container(
+        color: Colors.green,
+        child: Align(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(width: 20),
+              Icon(Icons.check, color: Colors.white),
+              Text(" Complete",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w700)),
+            ],
+          ),
+          alignment: Alignment.centerLeft,
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.red,
+        child: Align(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(Icons.delete, color: Colors.white),
+              Text(" Delete",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w700)),
+              SizedBox(width: 20),
+            ],
+          ),
+          alignment: Alignment.centerRight,
+        ),
+      ),
+      child: GestureDetector(
+        onTap: onGoToReminderDetails,
+        child: Card(
+          elevation: 0,
+          color: reminderAccentColor.withOpacity(0.2),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
                           Text(
-                            widget.reminder.description ?? '',
+                            widget.reminder.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
                             overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
+                            maxLines: 1,
                           ),
-                      ],
+                          if (widget.reminder.description != null)
+                            Text(
+                              widget.reminder.description ?? '',
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                            ),
+                        ],
+                      ),
+                    ),
+                    Checkbox(
+                      value: widget.reminder.isDone,
+                      onChanged: (value) {
+                        onMarkReminderAsDone();
+                      },
+                      activeColor: reminderAccentColor,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 4),
+                if (widget.reminder.tags != null)
+                  Wrap(
+                    spacing: 5,
+                    runSpacing: 5,
+                    children: [
+                      ...widget.reminder.tags!,
+                    ].take(10).map((tag) {
+                      return Container(
+                        // margin: const EdgeInsets.only(right: 5),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 4,
+                        ),
+                        decoration: BoxDecoration(
+                          color: reminderAccentColor.withOpacity(0.4),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(
+                          '#$tag',
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                Container(
+                  padding: const EdgeInsets.only(top: 3),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    DateFormat.yMMMEd().add_Hm().format(widget.reminder.date),
+                    style: const TextStyle(
+                      fontSize: 11,
                     ),
                   ),
-                  Checkbox(
-                    value: widget.reminder.isDone,
-                    onChanged: (value) {
-                      onMarkReminderAsDone();
-                    },
-                    activeColor: reminderAccentColor,
-                  )
-                ],
-              ),
-              const SizedBox(height: 4),
-              if (widget.reminder.tags != null)
-                Wrap(
-                  spacing: 5,
-                  runSpacing: 5,
-                  children: [
-                    ...widget.reminder.tags!,
-                  ].take(10).map((tag) {
-                    return Container(
-                      // margin: const EdgeInsets.only(right: 5),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 4,
-                      ),
-                      decoration: BoxDecoration(
-                        color: reminderAccentColor.withOpacity(0.4),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Text(
-                        '#$tag',
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              Container(
-                padding: const EdgeInsets.only(top: 3),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  DateFormat.yMMMEd().add_Hm().format(widget.reminder.date),
-                  style: const TextStyle(
-                    fontSize: 11,
-                  ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
